f9c4af04ef127faf97045190beb659f3480285c9
Munteanu01 <munteanualexandru0101@gmail.com>
2024-04-09 17:31:04 +0300

    adding tailwind component

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,8 @@
 <x-layout>
+    <x-slot:heading>
+        About Heading
+    </x-slot:heading>
+
+
     <h1>About</h1>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
+    <x-slot:heading>
+        Home Heading
+    </x-slot:heading>
+
     <h1>Home</h1>
 </x-layout>
