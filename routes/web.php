2b6aea5deffc5e2627773c1ebcb984012815107b
Munteanu01 <munteanualexandru0101@gmail.com>
2024-04-10 11:31:17 +0300

    fixing

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-
+Route::get('/about', function () {
+    return view('about');
+});
 Route::get('/jobs', function () {
     return view('jobs', [
         'greeting'=> 'Hello',
         'name'=> 'Alex',
         'jobs'=> [
             ['title' => 'Senior Developer',
-            'id' => '1',
             'salary' => '6000'],
             ['title'=> 'Junior Developer',
-            'id' => '2',
             'salary' => '4000'],
             ['title'=> 'AI Developer',
-            'id' => '3',
             'salary'=> '5000'],
         ],
     ]);
