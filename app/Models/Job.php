7fa9054668078b43c6a9d24b4f4959bd8dfa4d5e
Munteanu01 <munteanualexandru0101@gmail.com>
2024-04-10 23:10:44 +0300

    adding factory and model from database

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,31 +1,14 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 
 
-class Job {
-    public static function all(): array 
-    { return [
-        ['title' => 'Senior Developer',
-        'id' => '1',
-        'salary' => '6000'],
-        ['title'=> 'Junior Developer',
-        'id' => '2',
-        'salary' => '4000'],
-        ['title'=> 'AI Developer',
-        'id' => '3',
-        'salary'=> '5000'],
-    ];
-    }
-    public static function find(int $id): array
-    { 
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-        if (! $job) {
-            abort(404);
-        }
-        return $job;
-    }
+class Job extends Model{
+    use HasFactory;
+    protected $table = "job_listings";
+    protected $fillable = ['title', 'salary'];
 }
 
---
+++ database/factories/JobFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+class JobFactory extends Factory
+{
+
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->jobTitle(),
+            'salary' => fake()->numberBetween(100, 100.000),
+        ];
+    }
+}
